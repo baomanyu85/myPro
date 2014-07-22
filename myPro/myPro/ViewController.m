9288188e1c525f75019bd9849d48d6a2ad6605cd
Jone zhang <baomy@icesmart.cn>
2014-07-22 11:36:07 +0800

    das123

---
+++ myPro/myPro/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  myPro
+//
+//  Created by baomanyu on 14-7-22.
+//  Copyright (c) 2014年 aa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"-----hello world!!--------");
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
